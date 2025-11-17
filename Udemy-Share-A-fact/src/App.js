5b40732a58e94f2f3023523992e47434d2c3f81b
amit reuveni <amitr123r@gmail.com>
2025-11-17 21:05:10 +0200

    add useEffect add Loader function

--- Udemy-Share-A-fact/src/App.js
+++ Udemy-Share-A-fact/src/App.js
@@ -1,57 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import supabase from "./supabase";
 import "./style.css";
 
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+// const initialFacts = [
+//   {
+//     id: 1,
+//     text: "React is being developed by Meta (formerly facebook)",
+//     source: "https://opensource.fb.com/",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+//     source:
+//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+//     category: "society",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "Lisbon is the capital of Portugal",
+//     source: "https://en.wikipedia.org/wiki/Lisbon",
+//     category: "society",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
 
 function App() {
 
   const [showForm, SetShowForm] = useState(false);
+  const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function getFacts() {
+      setIsLoading(true);
+      const { data: facts, error } = await supabase.
+        from('facts').select('*').order("text", {
+          ascending:
+            true
+        }).limit(1000);
+
+        console.log(error)
+
+        if(!error) setFacts(facts);
+        else alert("There was a problem getting data");
+      setIsLoading(false);
+    }
+    getFacts();
+  }, []);
 
   return (
     <>
       <Header showForm={showForm} SetShowForm={SetShowForm} />
 
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (<NewFactForm setFacts={setFacts} SetShowForm={SetShowForm} />) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactsList />
+        {isLoading ? <Loader /> : <FactsList facts=
+          {facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">Loading...</p>
 }
 
 function Header({ showForm, SetShowForm }) {
@@ -86,17 +112,57 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
 
-function NewFactForm() {
+
+function NewFactForm({ setFacts, SetShowForm }) {
 
   const [text, setText] = useState("");
-  const [source, setSource] = useState("");
+  const [source, setSource] = useState("http://example.com");
   const [category, setCategory] = useState("");
   const textLength = text.length;
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
+    // 1. Prevent browser reload
     e.preventDefault();
     console.log(text, source, category);
+
+    // 2. Check if data is valid. if so, create a new fact.
+
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {
+
+      // 3. Create a new fact object.
+      const newFact = {
+        id: Math.round(Math.random() * 1000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      // 4. Add the new fact to the UI: add the fact to state.
+      setFacts((facts) => [newFact, ...facts]);
+
+      // 5. Reset input fields.
+      setText("");
+      setSource("");
+      setCategory("");
+
+      // 6. Close the form.
+      SetShowForm(false);
+
+    }
   }
 
   return (
@@ -150,9 +216,8 @@ function CategoryFilter() {
   )
 }
 
-function FactsList() {
-  // TEMPORARY
-  const facts = initialFacts;
+function FactsList({ facts }) {
+
 
   return (
     <section>
